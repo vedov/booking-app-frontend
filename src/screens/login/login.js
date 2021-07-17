faa466614ddbaad2f19dfa3afbbf7ae0f3c5644f
Vedad <vedad@procerasoft.com>
2021-07-17 20:24:12 +0200

    Login Page #2

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import Form from "../../components/form/form";
+import LoginConstants from "../../constants/login";
+import { login } from "./loginAction";
+import { useDispatch } from "react-redux";
+import Button from "../../components/button/button";
+import useMediaQuery from "../../useMediaQuery";
 
 const Login = () => {
-  return <div className="login"></div>;
+  const history = useHistory();
+  const mobile = useMediaQuery("(max-width: 768px)");
+  //const dispatch = useDispatch();
+  return (
+    <div className="login">
+      {mobile && (
+        <Button click={() => history.push("/")} variant="8">
+          <i class="fa fa-chevron-left"></i>
+        </Button>
+      )}
+      <div className="left">
+        {!mobile && (
+          <Button click={() => history.push("/")} variant="8">
+            <i class="fa fa-chevron-left"></i>
+            Back to Home
+          </Button>
+        )}
+      </div>
+
+      <Form
+        handleLogin={(data) => {
+          //dispatch(login({ email: data[0].value, password: data[1].value }));
+          console.log({ email: data[0].value, password: data[1].value });
+        }}
+        data={LoginConstants}
+      ></Form>
+    </div>
+  );
 };
 
 export default Login;
